90830bc0662ceeaf29c1856e8fa18d4383a12ce2
br12345678 <mystyle730@gmail.com>
2022-06-24 01:50:58 +0900

    0624 update

--- Java/InflearnJavaStudy/Array/Ex_0101.java
+++ Java/InflearnJavaStudy/Array/Ex_0101.java
@@ -1,4 +1,48 @@
 package Java.InflearnJavaStudy.Array;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Ex_0101 {
+    //위처럼 int 배열로 안하는 이유는
+    //ArrayList로하면 추가 되기 때문에 0이 찍혀서 0을 걸러낼 필요가없다.
+//    public int[] solution(int[] array, int num) {
+//        //6
+//        //7 3 9 5 6 12
+//        int[] answer = new int[num];
+//        answer[0] = array[0];
+//        for(int i=1; i<num; i++) {
+//            if(array[i-1] < array[i]) {
+//                answer[i]=array[i];
+//            }
+//        }
+//        return answer;
+//    }
+    public ArrayList<Integer> solution(int[] array, int num) {
+        //6
+        //7 3 9 5 6 12
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(array[0]);
+        for(int i=1; i<num; i++) {
+            if(array[i-1] < array[i]) {
+                answer.add(array[i]);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Ex_0101 T = new Ex_0101();
+        Scanner in=new Scanner(System.in);
+        int i = in.nextInt();
+        int[] array = new int[i];
+        for(int a=0; a<i; a++) {
+            array[a] = in.nextInt();
+        }
+        for(int x : T.solution(array,i)) {
+//            if(x != 0 )
+            System.out.print(x+" ");//이 부분을 ' '라고 했더니 이상한 숫자가 찍힌다. 주의할것
+        }
+        return ;
+    }
 }
--- Java/InflearnJavaStudy/String/Ex_0112_01.java
+++ Java/InflearnJavaStudy/String/Ex_0112_01.java
@@ -1,57 +1,53 @@
 package Java.InflearnJavaStudy.String;
 
-import java.util.Collections;
 import java.util.Scanner;
 
-public class Ex_0112 {
+public class Ex_0112_01 {
     public String solution(String str, int num) {
         //4
         //#****###**#####**#####**##**
         //#****## #**#### #**#### #**##**
         String answer = "";
-        int[] DigitArray = new int[str.length()];
+        int[] DigitArray = new int[num];
         String[] tmpArray = new String[num];
         char[] chaArray = str.toCharArray();
         int len = str.length()/num;
         int i=0;
 
-        //2진수로 변환
-        for(int a=0; a<str.length(); a++) {
-            if(chaArray[a] == '#') {
-                DigitArray[a]=1;
-            }else {
-                DigitArray[a]=0;
-            }
-        }
-
-        //len만큼 잘라서 넣어주기
+        //2진수로 변환 && len만큼 잘라서 넣어주기
         for(char x : str.toCharArray()) {
             if(tmpArray[i] == null) { //String배열의 null값을 처리하기위한 조건
-                tmpArray [i] = String.valueOf(x);
+                if(x == '#') {
+                    tmpArray[i] = String.valueOf('1');
+                } else {
+                    tmpArray[i] = String.valueOf('0');
+                }
             }
             else {
-                tmpArray[i] +=x;
+                if(x == '#') {
+                    tmpArray[i] += '1';
+                } else {
+                    tmpArray[i] += '0';
+                }
             }
             if(tmpArray[i].length()==len) {
                 i++;
             }
         }
-        
         //10진수로 변환
-        
-        //아스키코드로 문자 변환
-
-        for(int j=0; j<tmpArray.length; j++) {
-            System.out.println(tmpArray[j]);
+        for(int j=0; j<DigitArray.length; j++) {
+            DigitArray[j] = Integer.valueOf(tmpArray[j],2);
         }
-        System.out.println(len);
-
-
+        //아스키코드로 문자 변환
+        for(int a=0; a<DigitArray.length; a++) {
+            char tmp = (char)DigitArray[a];
+            answer+=String.valueOf(tmp);
+        }
         return answer;
     }
 
     public static void main(String[] args){
-        Ex_0112 T = new Ex_0112();
+        Ex_0112_01 T = new Ex_0112_01();
         Scanner in=new Scanner(System.in);
         int i = in.nextInt();
         String s = in.next();
