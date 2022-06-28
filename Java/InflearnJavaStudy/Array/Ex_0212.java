27322b49ae8ab2cb225a10494c112d9d0ca4ee67
br12345678 <mystyle730@gmail.com>
2022-06-29 02:15:19 +0900

    0629 update

--- Java/InflearnJavaStudy/Array/Ex_0212.java
+++ Java/InflearnJavaStudy/Array/Ex_0212.java
@@ -9,7 +9,29 @@ public class Ex_0212 {
         //4 3 2 1
         //3 1 4 2
         int answer = 0;
-
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                int cnt = 0;
+                for(int k=0; k<m; k++) {
+                    int pi = 0;
+                    int pj = 0;
+                    for(int s=0; s<n; s++) {
+                        if(array[k][s] == i) {
+                            pi = s;
+                        }
+                        if(array[k][s] == j) {
+                            pj = s;
+                        }
+                    }
+                    if(pi < pj) {
+                        cnt++;
+                    }
+                }
+                if(cnt == m) {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 
@@ -18,9 +40,9 @@ public class Ex_0212 {
         Scanner in=new Scanner(System.in);
         int i = in.nextInt();
         int j = in.nextInt();
-        int[][] array = new int[i][j];
-        for(int a=0; a<i; a++) {
-            for(int b=0; b<j; b++) {
+        int[][] array = new int[j][i];
+        for(int a=0; a<j; a++) {
+            for(int b=0; b<i; b++) {
                 array[a][b] = in.nextInt();
             }
         }
