d94b6531353cdb65976c6f90ee64e5adb63d33ff
br12345678 <mystyle730@gmail.com>
2022-07-24 22:31:36 +0900

    0724 update

--- Java/InflearnJavaStudy/HashMap_TreeSet/Ex_0402.java
+++ Java/InflearnJavaStudy/HashMap_TreeSet/Ex_0402.java
@@ -30,6 +30,25 @@ public class Ex_0402 {
         return "NO";
     }
 
+    public String solution2(String str1, String str2) {
+        //AbaAeCe
+        //baeeACA
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char a : str1.toCharArray()) {
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        for(char b : str2.toCharArray()) {
+            if(!map.containsKey(b) || map.get(b) == 0) {//애초에 map에 있는것중 key가없는 것 or 0인것은 존재할 수가 없다.
+                return "NO";
+            } else {
+                map.put(b, map.get(b)-1);
+            }
+        }
+
+        return "YES";
+    }
+
     public static void main(String[] args){
         Ex_0402 T = new Ex_0402();
         Scanner in=new Scanner(System.in);
